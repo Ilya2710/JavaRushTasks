afd224723072d4af69669ff846955b5a66e88c98
Ilya2710 <ilya.smirnov82@gmail.com>
2023-05-26 18:46:57 +0300

    3.JavaMultithreading: task3608 (06)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class ModelData {
     private List<User> users;
+    private User activeUser;
+    private boolean displayDeletedUserList;
 
     public void setUsers(List<User> users) {
         this.users = users;
@@ -13,5 +15,21 @@ public class ModelData {
 
     public List<User> getUsers() {
         return users;
+    }
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
     }
 }
