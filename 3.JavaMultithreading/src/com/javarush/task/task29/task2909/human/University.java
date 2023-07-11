0452ecf46940a1b85366273a6de0dcc77171bdfd
Ilya2710 <ilya.smirnov82@gmail.com>
2023-07-12 02:04:09 +0300

    3.JavaMultithreading: task2909(04)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,9 +1,41 @@
 package com.javarush.task.task29.task2909.human;
 
-public class University extends Student {
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
+    protected String name;
+    protected int age;
+
+    private List<Student> students = new ArrayList<Student>();
 
     public University(String name, int age) {
-        super(name, age, 0);
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
     }
 
     public Student getStudentWithAverageGrade() {
