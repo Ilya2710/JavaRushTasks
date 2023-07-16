f24a90890f8ff835e0fb782307cca13e6168950f
Ilya2710 <ilya.smirnov82@gmail.com>
2023-07-17 00:17:59 +0300

    3.JavaMultithreading: task2909(09)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -12,19 +12,25 @@ public class Human implements Alive {
     protected int age;
     protected String name;
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public class Size {
+        public int height;
+        public int weight;
+
+        public Size(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
     }
 
-    public int getBloodGroup() {
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -71,7 +77,7 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public String getPosition(){
