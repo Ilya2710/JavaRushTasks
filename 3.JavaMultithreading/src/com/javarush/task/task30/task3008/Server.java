2bb52b1fba78be10206935c657f958392bc29003
Ilya2710 <ilya.smirnov82@gmail.com>
2025-07-17 01:39:39 +0300

    3.JavaMultithreading: task3008(06)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,39 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port;
+        ServerSocket serverSocket = null;
+
+        port = ConsoleHelper.readInt();
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started successfully.");
+
+            while(true) {
+                Socket client = serverSocket.accept();
+                Handler handler = new Handler(client);
+                handler.start();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        private Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
