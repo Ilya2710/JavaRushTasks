b6d776508190bc757bcce71604b05ab1c79a03d8
Ilya2710 <ilya.smirnov82@gmail.com>
2025-07-18 01:25:16 +0300

    3.JavaMultithreading: task3008(18)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client {
+
+    public static void main(String[] args) throws InterruptedException {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + ((int)(100*Math.random()));
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    public class BotSocketThread extends SocketThread {
+
+    }
+}
